bf5b742cb31bca395d691f9e20d550f70e7136f6
“kukim” <kukim.dev@gmail.com>
2022-12-13 17:07:16 +0900

    Solution 2.2: DB 활용 - OptimisticLock
    - 낙관적 락
    - version 컬럼으로 최신 데이터 확인
    - 장점 : 락을 걸지 않기 때문에 성능이 좋음
    - 단점 : 업데이트 실패 시 로직을 직접 구현해야함 / 충돌 빈번하게 발생한다면 Pessimistic 방법이 효과적일 수 있음

---
+++ src/test/java/dev/kukim/stock/fasade/OptimisticLockStockFacadeTest.java
@@ -0,0 +1,74 @@
+package dev.kukim.stock.fasade;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import dev.kukim.stock.domain.Stock;
+import dev.kukim.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@DisplayName("OptimisticLockStockFacadeTest 클래스")
+@SpringBootTest
+class OptimisticLockStockFacadeTest {
+
+	@Autowired
+	private OptimisticLockStockFacade optimisticLockStockFacade;
+
+	@Autowired
+	private StockRepository stockRepository;
+
+	@BeforeEach
+	void setUp() {
+		Stock stock = new Stock(1L, 100L);
+
+		stockRepository.saveAndFlush(stock);
+	}
+
+	@AfterEach
+	void tearDown() {
+		stockRepository.deleteAll();
+	}
+
+	@Test
+	void 싱글스레드_재고수량_한개_감소_테스트() throws InterruptedException {
+		optimisticLockStockFacade.decrease(1L, 1L);
+
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+
+		assertThat(stock.getQuantity()).isEqualTo(99L);
+	}
+
+	@Test
+	void 멀티스레드_동시에_100개_재고수량_감소_테스트() throws InterruptedException {
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					optimisticLockStockFacade.decrease(1L, 1L);
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+
+		assertThat(stock.getQuantity()).isEqualTo(0L);
+	}
+
+
+}
