2566953b12fba98d436523f6b422e3a222819944
“kukim” <kukim.dev@gmail.com>
2022-12-13 15:54:54 +0900

    version 1의 문제점: 멀티스레드로 동시 작업 시 Race Condition 발생한다.

--- src/test/java/dev/kukim/stock/service/StockServiceTest.java
+++ src/test/java/dev/kukim/stock/service/StockServiceTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import dev.kukim.stock.domain.Stock;
 import dev.kukim.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -43,4 +46,26 @@ class StockServiceTest {
 		assertThat(stock.getQuantity()).isEqualTo(99L);
 	}
 
+	@Test
+	void 멀티스레드_동시에_100개_재고수량_감소_테스트() throws InterruptedException {
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+
+		assertThat(stock.getQuantity()).isEqualTo(0L);
+	}
 }
